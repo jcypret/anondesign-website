f0edb1d9e8cf229d2560837bbb358048d52738e6
Justin Cypret <jcypret@gmail.com>
2015-03-12 22:23:51 -0500

    Configure cloudfront to invalidate html

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'middleman-autoprefixer', '~> 2.4.3'
 gem 'middleman-title', '~> 0.0.2'
 gem 'middleman-search_engine_sitemap', '~> 1.3.0'
 gem 'middleman-s3_sync', '~> 3.0.42'
+gem 'middleman-cloudfront', '~> 0.1.1'
 
 gem 'jeet', '~> 6.1.2'
 gem 'breakpoint', '~> 2.5.0'
--- config.rb
+++ config.rb
@@ -8,7 +8,7 @@ set :partials_dir, 'partials'
 
 activate :title, site: 'Anonymous Design'
 
-set :url_root, 'http://anondesign.co'
+set :url_root, 'https://anondesign.co'
 activate :search_engine_sitemap
 
 sprockets.append_path File.join(root, 'bower_components')
@@ -39,3 +39,15 @@ activate :s3_sync do |s3_sync|
   s3_sync.after_build = true
 end
 caching_policy 'text/html', max_age: 0, must_revalidate: true
+
+activate :cloudfront do |cf|
+  cf.access_key_id = ENV['AWS_ACCESS_KEY_ID']
+  cf.secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+  cf.distribution_id = ENV['AWS_CLOUDFRONT_ID']
+  cf.filter = /\.html$/i
+  cf.after_build = true
+end
+
+after_s3_sync do |files_by_status|
+  invalidate files_by_status[:updated]
+end
