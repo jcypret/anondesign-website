2500977c854b55c048797fd8f9de569daad6727f
Justin Cypret <jcypret@gmail.com>
2016-03-10 22:08:35 -0600

    Remove old deployment scripts

--- Gemfile
+++ Gemfile
@@ -7,8 +7,6 @@ gem 'middleman-sprockets', '~> 3.2.0'
 gem 'middleman-autoprefixer', '~> 2.4.3'
 gem 'middleman-title', '~> 0.0.2'
 gem 'middleman-search_engine_sitemap', '~> 1.3.0'
-gem 'middleman-s3_sync', '~> 3.0.42'
-gem 'middleman-cloudfront', '~> 0.1.1'
 
 gem 'breakpoint', '~> 2.5.0'
 gem 'dotenv', '~> 2.0.0'
--- config.rb
+++ config.rb
@@ -33,21 +33,3 @@ configure :build do
   activate :relative_assets
   activate :gzip
 end
-
-activate :s3_sync do |s3_sync|
-  s3_sync.bucket = 'anondesign.co'
-  s3_sync.after_build = true
-end
-caching_policy 'text/html', max_age: 0, must_revalidate: true
-
-activate :cloudfront do |cf|
-  cf.access_key_id = ENV['AWS_ACCESS_KEY_ID']
-  cf.secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-  cf.distribution_id = ENV['AWS_CLOUDFRONT_ID']
-  cf.filter = /\.html$/i
-  cf.after_build = true
-end
-
-after_s3_sync do |files_by_status|
-  invalidate files_by_status[:updated]
-end
